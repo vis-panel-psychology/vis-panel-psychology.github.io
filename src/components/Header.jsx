d38482e2cc5292c481aa6173d5f8a81a545cb6cf
arranzeyuwang <zywangx@gmail.com>
2025-08-28 16:03:22 -0400

    feat: update styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,10 +13,10 @@ const Header = () => {
 		<div>
 			<img src="./imgs/vis2025_logo.svg" alt="IEEE VIS 2025 Logo" className={styles.logo} />
 			<div className={styles.headerWrapper}>
-				<h2>{parse("What Does Psychology Really Mean to Visualization Research?")}</h2>
-				<h3>{"Towards PsychXVis and VisXPsych"}</h3>
+				<h2>{parse("What Does Psychology Really Mean to Visualization Research? ")}<span  className={styles.smallHeader}>{"Towards PsychXVis and VisXPsych"}</span></h2>
 			</div>
 				<h3>{"Panel @ IEEE VIS 2025"}</h3>
+				<h3 className={styles.dateWrapper}>{"Date, 2025 Time CEST (GMT+2)"}</h3>
 			<p>
 				{parse("This panel aims to discuss the critical yet underdeveloped relationship between psychology and visualization (VIS) research. While VIS frequently draws upon psychological principles—particularly in perception and cognition—current integration remains shallow, often relying on heuristics and anecdotal evidence rather than rigorous scientific foundations. This gap hinders the development of generalizable design principles, limits consideration of human factors, and risks creating technically advanced but ineffective or misleading visualizations. Simultaneously, psychology's growing interest in using visualization as an experimental tool presents future potential for bidirectional collaboration.")}
 			</p>
