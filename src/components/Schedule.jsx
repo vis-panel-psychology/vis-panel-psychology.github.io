d265a1fbffe2092d00c39ad45c7ba3c776ced319
arranzeyuwang <zywangx@gmail.com>
2025-09-26 17:19:10 -0400

    docs: add tentative presentations

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -3,18 +3,18 @@ import styles from "./Schedule.module.scss";
 
 const schedule = [
   { time: "3:45 PM - 3:50 PM", event: "Introduction by Organizers" },
-  { time: "3:50 PM - 3:55 PM", event: "Presentation by Panelist", bold: "Panelist" },
-  { time: "3:55 PM - 4:00 PM", event: "Presentation by Panelist", bold: "Panelist" },
-  { time: "4:00 PM - 4:05 PM", event: "Presentation by Panelist", bold: "Panelist" },
-  { time: "4:05 PM - 4:10 PM", event: "Presentation by Panelist", bold: "Panelist" },
-  { time: "4:10 PM - 4:15 PM", event: "Presentation by Panelist", bold: "Panelist" },
+  { time: "3:50 PM - 3:55 PM", event: "Presentation by Min Chen", bold: "Min Chen" },
+  { time: "3:55 PM - 4:00 PM", event: "Presentation by Alex Kale", bold: "Alex Kale" },
+  { time: "4:00 PM - 4:05 PM", event: "Presentation by Alvitta Ottley", bold: "Alvitta Ottley" },
+  { time: "4:05 PM - 4:10 PM", event: "Presentation by Lace Padilla", bold: "Lace Padilla" },
+  { time: "4:10 PM - 4:15 PM", event: "Presentation by Karen Schloss", bold: "Karen Schloss" },
   { time: "4:15 PM - 4:55 PM", event: "Panel Discussion and Q & A" },
   { time: "4:55 PM - 5:00 PM", event: "Closing by Organizers" }
 ];
 
 const Schedule = () => (
   <div className={styles.scheduleBlock}>
-    <h3 className={styles.scheduleTitle}>Schedule</h3>
+    <h3 className={styles.scheduleTitle}>Schedule (tentative)</h3>
     <table className={styles.scheduleTable}>
       <thead>
         <tr>
